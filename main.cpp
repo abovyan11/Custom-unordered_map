d02f2a39e24e29e3a9b39e887103dbc50aca0c48
Garik Abovyan <garik.abovyan@inbox.ru>
2023-02-26 14:59:19 +0400

    second commit

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ using namespace std::literals;
 //to work properly give right local dir before compiling
 const char* file_path = "C:/Users/User/Desktop/k_USA.txt";
 
-template<typename Key,typename Value, typename Container>
-void comparison(Container & param) {
+template<typename Key, typename Value, typename Container>
+void comparison(Container& param) {
     std::ifstream file;
     file.open(file_path);
     if (file.is_open()) {
@@ -19,26 +19,31 @@ void comparison(Container & param) {
         Value val{};
         using namespace std::chrono;
         high_resolution_clock::time_point t1 = high_resolution_clock::now();
-        while (file >> key >> val) {
+        //checking insert and erase
+        //inserting pairs from the file
+        while (file >> key >> val)
             param.insert({ key,val });
-        }
         param.erase(param.begin());
         high_resolution_clock::time_point t2 = high_resolution_clock::now();
+        //checking copy
         Container check_copy(param);
-        high_resolution_clock::time_point t3 = high_resolution_clock::now();
 
+        high_resolution_clock::time_point t3 = high_resolution_clock::now();
+        //checking find function
         auto it = param.find("ahhzz@yahoo.com");
 
         high_resolution_clock::time_point t4 = high_resolution_clock::now();
-        if(it!=param.end())
-            std::cout<<"found: "<<it->second<<"\n";
+
+        if (it != param.end())
+            std::cout << "found: " << it->second << "\n";
         else
-            std::cout<<"nothing is found.";
+            std::cout << "nothing is found.\n";
+
         std::cout << "elapsed time of filling: " << duration_cast<milliseconds>(t2 - t1).count() << "ms\n"
                   << "elapsed time of copying: " << duration_cast<milliseconds>(t3 - t2).count() << "ms\n"
-                  << "elapsed time of finding: " << duration_cast<nanoseconds>(t4 - t3).count() << "ns\n"
-                  << "elements in map: " << check_copy.size()<<"\nsizeof map: "<<sizeof(check_copy)<<"\nbuckets_count: "
-                  << check_copy.bucket_count()<<std::endl;
+                  << "elapsed time of finding: " << duration_cast<nanoseconds>(t4 - t3).count() <<  "ns\n"
+                  << "elements in map: "         << check_copy.size() << "\nsizeof map: " << sizeof(check_copy)
+                  << "\nbuckets_count: "         << check_copy.bucket_count() << std::endl;
     }
 }
 
@@ -46,11 +51,12 @@ void comparison(Container & param) {
 int main() {
 
     std::unordered_map<std::string, std::string> std_map;
-    my_unordered_map<std::string, std::string> my_map;
+    my_unordered_map  <std::string, std::string> my_map;
 
     std::cout << "inserting data to std_map. Result: \n";
-    comparison< std::string, std::string > (std_map);
+    comparison< std::string, std::string >(std_map);
     std::cout << "\ninserting data to my_map. Result: \n";
     comparison< std::string, std::string >(my_map);
+
     return 0;
 }
